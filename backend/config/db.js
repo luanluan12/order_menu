4337fd8be2f2eb8a51234dcbf1d02ef5b3348f06
Đức Luận <nguyenha@Mac-Luan.local>
2026-07-09 17:34:10 +0700

    product

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
+  try {
+    console.log("Connecting to Mongo...");
+    console.log(process.env.MONGO_URI);
 
-        await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
 
-        console.log("MongoDB Connected");
-
-    } catch (err) {
-
-        console.error("MongoDB connection failed:");
-        console.error(err);
-        process.exit(1);
-
-    }
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Mongo Error:");
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,71 +1,120 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
+// const transporter = nodemailer.createTransport({
 
-    host: process.env.MAIL_HOST,
+//     host: process.env.MAIL_HOST,
 
-    port: process.env.MAIL_PORT,
+//     port: process.env.MAIL_PORT,
 
-    secure: false,
+//     secure: false,
 
-    auth: {
+//     auth: {
 
-        user: process.env.MAIL_USER,
+//         user: process.env.MAIL_USER,
 
-        pass: process.env.MAIL_PASS
+//         pass: process.env.MAIL_PASS
 
-    }
+//     }
 
-});
+// });
 
-// const sendMail = async ({
+// // const sendMail = async ({
 
-//     to,
+// //     to,
 
-//     subject,
+// //     subject,
 
-//     html
+// //     html
 
-// }) => {
+// // }) => {
 
-//     return transporter.sendMail({
+// //     return transporter.sendMail({
 
-//         from: process.env.MAIL_FROM,
+// //         from: process.env.MAIL_FROM,
 
-//         to,
+// //         to,
 
-//         subject,
+// //         subject,
 
-//         html
+// //         html
 
-//     });
+// //     });
+
+// // };
+
+// const sendMail = async ({ to, subject, html }) => {
+//     console.log("sendMail called");
+
+//     try {
+
+//         const info = await transporter.sendMail({
+//             from: process.env.MAIL_FROM,
+//             to,
+//             subject,
+//             html
+//         });
+
+//         console.log("Mail sent:", info.messageId);
+
+//         return info;
+
+//     } catch (err) {
+
+//         console.error("Send mail error:", err);
+
+//         throw err;
+
+//     }
 
 // };
+
+// module.exports = sendMail;
+
+const dns = require("node:dns");
+const nodemailer = require("nodemailer");
+
+// Ưu tiên IPv4
+dns.setDefaultResultOrder("ipv4first");
+
+console.log("Mail Config:", {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    user: process.env.MAIL_USER,
+});
+
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    secure: false,
+    family: 4,
+    logger: true,
+    debug: true,
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+    },
+});
 
 const sendMail = async ({ to, subject, html }) => {
     console.log("sendMail called");
 
     try {
-
         const info = await transporter.sendMail({
             from: process.env.MAIL_FROM,
             to,
             subject,
-            html
+            html,
         });
 
         console.log("Mail sent:", info.messageId);
-
         return info;
-
     } catch (err) {
-
         console.error("Send mail error:", err);
-
         throw err;
-
     }
-
 };
 
 module.exports = sendMail;
