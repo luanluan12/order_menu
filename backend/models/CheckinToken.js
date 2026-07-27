a23b9b109c07949049d856bb5709f315243e3320
Đức Luận <nguyenha@Mac-Luan.local>
2026-07-27 11:56:38 +0700

    fix in valid

--- backend/models/CheckinToken.js
+++ backend/models/CheckinToken.js
@@ -1,38 +1,33 @@
 const mongoose = require("mongoose");
 
 const checkinTokenSchema = new mongoose.Schema(
-    {
-
-        floor: {
-            type: Number,
-            required: true
-        },
-
-        token: {
-            type: String,
-            required: true
-        },
-
-        active: {
-            type: Boolean,
-            default: true
-        }
+  {
+    floor: {
+      type: Number,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+
+    token: {
+      type: String,
+      required: true,
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 checkinTokenSchema.index(
-    {
-        date: 1,
-        floor: 1
-    },
-    {
-        unique: true
-    }
+  {
+    floor: 1,
+  },
+  {
+    unique: true,
+  },
 );
 
-module.exports = mongoose.model(
-    "CheckinToken",
-    checkinTokenSchema
-);
+module.exports = mongoose.model("CheckinToken", checkinTokenSchema);
